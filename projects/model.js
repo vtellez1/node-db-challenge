b293fcfef4e418bd432cba374e5a97af4cc0f0bf
Vanessa <vanessatellez45@gmail.com>
2020-01-24 11:18:11 -0500

    set-up

---
+++ projects/model.js
@@ -0,0 +1,34 @@
+const db = require('');
+
+module.exports = {
+    getProjects,
+    addProject,
+    getTasks,
+    addTask,
+    getResources,
+    addResource
+}
+
+function getProjects(){
+    return
+}
+
+function addProject(){
+    return
+}
+
+function getTasks(){
+    return
+}
+
+function addTask(){
+    return
+}
+
+function getResources(){
+    return
+}
+
+function addResource(){
+    return
+}
---
+++ projects/project-router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const Projects = require('./model.js');
+
+const router = express.Router();
+
+// Insert Endpoints here
+
+module.exports = router;
